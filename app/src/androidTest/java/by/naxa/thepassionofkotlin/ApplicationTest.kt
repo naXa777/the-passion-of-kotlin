ca80f13f00363f059eb8815e1d9fa1a1eb501c66
naXa777 <alarmo.lord@gmail.com>
2015-07-10 11:39:09 +0300

    Initial commit.

---
+++ app/src/androidTest/java/by/naxa/thepassionofkotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package by.naxa.thepassionofkotlin
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+public class ApplicationTest : ApplicationTestCase<Application>(javaClass<Application>())
