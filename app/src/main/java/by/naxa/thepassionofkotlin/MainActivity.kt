358927f4dbff36abf0408ce6b113ecd27ded143a
naXa777 <alarmo.lord@gmail.com>
2015-07-10 18:02:32 +0300

    Main activity: initialization + RecyclerView with Swipe-on-Refresh feature.

--- app/src/main/java/by/naxa/thepassionofkotlin/MainActivity.kt
+++ app/src/main/java/by/naxa/thepassionofkotlin/MainActivity.kt
@@ -1,16 +1,34 @@
 package by.naxa.thepassionofkotlin
 
-import kotlinx.android.synthetic.activity_main.*;
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import by.naxa.thepassionofkotlin.adapter.StudentRecyclerAdapter
+import by.naxa.thepassionofkotlin.model.Student
+import by.naxa.thepassionofkotlin.network.request.GetListStudentSpiceRequest
+import by.naxa.thepassionofkotlin.network.service.TheUniversityAPISpiceService
+import com.octo.android.robospice.SpiceManager
+import com.yoavst.kotlin.v
+import de.greenrobot.event.EventBus
+import kotlinx.android.synthetic.activity_main.mainRecyclerView
+import kotlinx.android.synthetic.activity_main.mainSwipeRefreshLayout
+import kotlinx.android.synthetic.activity_main.toolbar
+import kotlin.properties.Delegates
 
 public class MainActivity : AppCompatActivity() {
 
+    val mSpiceMgr : SpiceManager = SpiceManager(javaClass<TheUniversityAPISpiceService>())
+
+    var mAdapter = StudentRecyclerAdapter();
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super<AppCompatActivity>.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUp()
+        v("::onCreate()")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,4 +50,52 @@ public class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onResume() {
+        super<AppCompatActivity>.onResume()
+        EventBus.getDefault().register(this)
+        v("::onResume()")
+    }
+
+    override fun onPause() {
+        super<AppCompatActivity>.onPause()
+        EventBus.getDefault().unregister(this)
+        v("::onPause()")
+    }
+
+    override fun onStart() {
+        super<AppCompatActivity>.onStart()
+        mSpiceMgr.start(this)
+    }
+
+    override fun onStop() {
+        super<AppCompatActivity>.onStop()
+        mSpiceMgr.shouldStop()
+    }
+
+    fun getServiceSpiceManager(): SpiceManager {
+        return mSpiceMgr
+    }
+
+    private fun setUp() {
+        setSupportActionBar(toolbar)
+        toolbar.setTitle("Students")
+
+        mainRecyclerView.setLayoutManager(createLayoutManager());
+        mainRecyclerView.setAdapter(mAdapter)
+        mainSwipeRefreshLayout.setOnRefreshListener{
+            refreshContent()
+            mainSwipeRefreshLayout.setRefreshing(false);
+        }
+    }
+
+    private fun refreshContent() {
+        mAdapter = StudentRecyclerAdapter(GetListStudentSpiceRequest().loadDataFromNetwork())
+        mainRecyclerView.setAdapter(mAdapter);
+    }
+
+    private fun createLayoutManager(): RecyclerView.LayoutManager {
+        return LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
 }
